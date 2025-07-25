294b4db8717cce6d0c1d522592a9a75bfbeb5efb
ARYAN <aryan.70921@gmail.com>
2025-07-25 20:13:25 +0530

    update backend

--- backend/db/connectToMongoDB.js
+++ backend/db/connectToMongoDB.js
@@ -1,12 +1,26 @@
-import mongoose from "mongoose";
 
-const connectToMongoDB = async () => {
+import mongoose from 'mongoose'
+
+// const db=async()=>{
+// 	console.log(process.env.MONGO_URI,"LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL")
+// mongoose.connect(process.env.MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// }).then(() => {
+//   console.log("MongoDB connected");
+// }).catch((err) => {
+//   console.log("Error connecting to MongoDB", err);
+// });
+// }
+
+const connectDb = async () => {
 	try {
-		const conn=await mongoose.connect(process.env.MONGO_URI);
-		console.log("Connected to MongoDB",conn.connection.host);
-	} catch (error) {
-		console.log("Error connecting to MongoDB", error.message);
+		const conn = await mongoose.connect(process.env.MONGO_URI);
+		console.log("Connected Mongoose:", conn.connection.host);
 	}
-};
+	catch (error) {
+		console.log("Error in connecting MongoDb", error);
+	}
+}
 
-export default connectToMongoDB;
+export default connectDb;
